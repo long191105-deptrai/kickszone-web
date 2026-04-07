efb36c1b1dc87e606617a2ffcebc5ce753be4158
long191105-deptrai <nguyenlong19112005@gmail.com>
2026-04-08 02:36:54 +0700

    Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useCart } from '../context/CartContext'; 
+import { toast } from 'react-toastify'; // Thêm dòng này vào bác nhé
 
 // Import logo
 import logoKicksZone from '../assets/images/logo.png'; 
